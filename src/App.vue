a8eea6bffe5e22737226c265def55b47b0b8a11d
Ruben Pierich <rpierich@hotmail.com>
2020-04-02 20:33:25 -0400

    Added code to be edited.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,149 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <v-app id="inspire">
+
+            <v-app-bar
+                    :clipped-left="$vuetify.breakpoint.lgAndUp"
+                    app
+                    color="blue darken-3"
+                    dark
+            >
+                <v-toolbar-title
+                        style="width: 300px"
+                        class="ml-0 pl-3"
+                >
+                    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+                    <span class="hidden-sm-and-down"><a href="/" style="color: white;text-decoration: none">Weight Tracking Application</a></span>
+                </v-toolbar-title>
+                <v-text-field
+                        flat
+                        solo-inverted
+                        hide-details
+                        prepend-inner-icon="search"
+                        label="Search"
+                        class="hidden-sm-and-down"
+                ></v-text-field>
+                <v-spacer></v-spacer>
+
+                <v-btn icon>
+                    <v-icon>notifications</v-icon>
+                </v-btn>
+                <v-btn
+                        icon
+                        large
+                >
+                    <v-avatar
+                            size="32px"
+                            item
+                    >
+                        <v-img
+                                src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+                                alt="Vuetify"
+                        >
+                        </v-img>
+                    </v-avatar>
+                </v-btn>
+                <v-btn icon v-on:click="logOut">
+                    <v-icon>power_settings_new</v-icon>
+                </v-btn>
+            </v-app-bar>
+            <v-content>
+
+                <v-container
+                        fluid
+                >
+
+                    <v-layout
+                            align-center
+                            justify-center
+                    >
+
+
+                        <v-row no-gutters style="height: 500px;">
+                            <v-col
+                            >
+                                <v-card
+                                        class="pa-2"
+                                        tile
+                                        outlined
+                                >
+                                    <transition name="component-fade" mode="out-in">
+                                        <div>
+
+                                            <router-view></router-view>
+                                            <component :is="currentView" v-on:goToPosts="goToNextScreen"/>
+                                        </div>
+                                    </transition>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+
+
+                        <br/><br/>
+                        <v-tooltip right>
+                            <template v-slot:activator="{ on }">
+                                <v-btn
+                                        :href="source"
+                                        icon
+                                        large
+                                        target="_blank"
+                                        v-on="on"
+                                >
+
+                                </v-btn>
+                            </template>
+
+                        </v-tooltip>
+
+                    </v-layout>
+                </v-container>
+            </v-content>
+        </v-app>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import SelectUser from './components/SelectUser'
+    import {store} from './components/UserStore'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        store,
+        data: () => ({
+            currentView: SelectUser,
+        }),
+        components: {},
+        watch: {
+            // eslint-disable-next-line no-unused-vars
+            '$route'(to, from) {
+            }
+        },
+        mounted: function () {
+        },
+        props: {
+            source: String
+        },
+        methods: {
+            goToNextScreen(screen) {
+                this.currentView = screen
+            },
+            logOut() {
+                if (this.currentView !== SelectUser) {
+                    this.goToNextScreen(SelectUser)
+                    this.$store.dispatch("deleteUser", {});
+                }
+                // this.goToNextScreen(EnterWeightForm);
+            }
+        }
+
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .component-fade-enter-active, .component-fade-leave-active {
+        transition: opacity 0.2s ease;
+    }
+
+    .component-fade-enter, .component-fade-leave-to {
+        opacity: 0;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,51 @@
+// src/main.js
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import router from './router/index'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App),
-}).$mount('#app')
+  vuetify: vuetify,
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    items: [
+      { icon: 'contacts', text: 'Contacts' },
+      { icon: 'history', text: 'Frequently contacted' },
+      { icon: 'content_copy', text: 'Duplicates' },
+      {
+        icon: 'keyboard_arrow_up',
+        'icon-alt': 'keyboard_arrow_down',
+        text: 'Labels',
+        model: true,
+        children: [
+          { icon: 'add', text: 'Create label' },
+        ],
+      },
+      {
+        icon: 'keyboard_arrow_up',
+        'icon-alt': 'keyboard_arrow_down',
+        text: 'More',
+        model: false,
+        children: [
+          { text: 'Import' },
+          { text: 'Export' },
+          { text: 'Print' },
+          { text: 'Undo changes' },
+          { text: 'Other contacts' },
+        ],
+      },
+      { icon: 'settings', text: 'Settings' },
+      { icon: 'chat_bubble', text: 'Send feedback' },
+      { icon: 'help', text: 'Help' },
+      { icon: 'phonelink', text: 'App downloads' },
+      { icon: 'keyboard', text: 'Go to the old version' },
+    ],
+  })
+})
